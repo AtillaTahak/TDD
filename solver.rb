0610458ea9bf94a9bfd34ef921fe45f9e25415e6
atillatahak <atillatahaa@gmail.com>
2022-03-18 17:17:50 +0300

    added factorial method and class which name solver

---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.negative?
+      raise ArgumentError
+    else
+      num * factorial(num - 1)
+    end
+  end
+end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,29 +1,28 @@
-require "../solver"
+require '../solver'
 
-
-describe "factorial" do
-    before(:each) do
-        @solver = Solver.new
-    end
-    it "returns 1 when given 0" do
-        expect(@solver.factorial(0)).to eq(1)
-    end
-    it "returns 1 when given 1" do
-        expect(@solver.factorial(1)).to eq(1)
-    end
-    it "returns 2 when given 2" do
-        expect(@solver.factorial(2)).to eq(2)
-    end
-    it "returns 6 when given 3" do
-        expect(@solver.factorial(3)).to eq(6)
-    end
-    it "returns 24 when given 4" do
-        expect(@solver.factorial(4)).to eq(24)
-    end
-    it "returns 120 when given 5" do
-        expect(@solver.factorial(5)).to eq(120)
-    end
-    it "return exception when given a negative number" do
-        expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
-    end
+describe 'factorial' do
+  before(:each) do
+    @solver = Solver.new
+  end
+  it 'returns 1 when given 0' do
+    expect(@solver.factorial(0)).to eq(1)
+  end
+  it 'returns 1 when given 1' do
+    expect(@solver.factorial(1)).to eq(1)
+  end
+  it 'returns 2 when given 2' do
+    expect(@solver.factorial(2)).to eq(2)
+  end
+  it 'returns 6 when given 3' do
+    expect(@solver.factorial(3)).to eq(6)
+  end
+  it 'returns 24 when given 4' do
+    expect(@solver.factorial(4)).to eq(24)
+  end
+  it 'returns 120 when given 5' do
+    expect(@solver.factorial(5)).to eq(120)
+  end
+  it 'return exception when given a negative number' do
+    expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+  end
 end
