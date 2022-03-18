4b2f05bb7d8c9078b983e9203af30d31c572641e
atillatahak <atillatahaa@gmail.com>
2022-03-18 17:50:07 +0300

    added fizzbuz method

--- solver.rb
+++ solver.rb
@@ -16,4 +16,16 @@ class Solver
       word.reverse
     end
   end
+
+  def fizzbuzz(num)
+    if num % 15 == 0
+      'fizzbuzz'
+    elsif num % 3 == 0
+      'fizz'
+    elsif num % 5 == 0
+      'buzz'
+    else
+      num
+    end
+  end
 end
